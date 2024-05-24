5c2d42a6fd1d4b2ae67dfeb3b3f18fe9f8133308
Yash <139779840+yashop7@users.noreply.github.com>
2024-05-24 15:38:19 +0530

    Error Fixed

---
+++ my-turborepo/apps/skizify/app/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+export default function Layout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }): JSX.Element {
+    return (
+      <div>
+              {children}  
+      </div>
+    );
+  }
--- my-turborepo/apps/skizify/app/page.tsx
+++ my-turborepo/apps/skizify/app/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
+  console.log(session);
   if(session?.user){
     redirect('/dashboard');
   }else{
