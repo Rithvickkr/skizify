e073c82b51493f91b08295a33a0a3a877650cba9
Yash <139779840+yashop7@users.noreply.github.com>
2024-06-04 15:48:24 +0530

    db updated

--- my-turborepo/apps/skizify/app/components/mygigs/Gig.tsx
+++ my-turborepo/apps/skizify/app/components/mygigs/Gig.tsx
@@ -2,7 +2,7 @@ import { useSession } from "next-auth/react";
 import { BentoGrid, BentoGridItem } from "./Bentogrid";
 import { Avatar } from "@repo/ui/avatar";
 import { getServerSession } from "next-auth";
-
+import { imageString} from "@repo/store"
 import prisma from "@repo/db/client";
 // import {getUserImage} from "../../lib/actions/getImage";
 import { authOptions } from "../../lib/auth";
@@ -10,8 +10,11 @@ import { authOptions } from "../../lib/auth";
 //This will fetch Data from DB
 
 export default async function GigStructure({image}:{image : string}) {
+
   // const image = await getUserImage();
-  console.log(image)
+  
+  console.log(image);
+
   return (
     <div>
       <BentoGrid>
--- my-turborepo/packages/store/src/count.ts
+++ my-turborepo/packages/store/src/count.ts
@@ -3,5 +3,5 @@ import { atom } from 'recoil';
 
 export const imageString = atom<string>({
   key: 'image',
-  default: '', 
+  default: 'hello', 
 });
