79f47930e70e079600c5d5ec1a4e72bd6d856c67
Rithvickkr <12216061@nitkkr.ac.in>
2024-06-05 02:05:07 +0530

    refactor: Update gig card display in mygigs page

--- my-turborepo/apps/skizify/app/(dashboard)/mygigs/page.tsx
+++ my-turborepo/apps/skizify/app/(dashboard)/mygigs/page.tsx
@@ -1,43 +1,75 @@
-import React from "react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../../../@/components/ui/card";
-import getgigs from "../../lib/actions/getgigs";
+// import React from "react";
+// import {
+//   Card,
+//   CardContent,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "../../../@/components/ui/card";
+// import getgigs from "../../lib/actions/getgigs";
 
-export default async function () {
-  const gigs: any = await getgigs();
+// export default async function () {
+//   const gigs: any = await getgigs();
+//   console.log(gigs);
+
+//   const formatTimeInterval = (hours: number, minutes: number) => {
+//     const totalMinutes = Math.floor(hours * 60 + minutes); // Convert hours to minutes and add the extra minutes
+//     const formattedHours = Math.floor(totalMinutes / 60); // Calculate total hours
+//     const formattedMinutes = totalMinutes % 60; // Get the remainder for minutes
+//     const paddedMinutes = String(formattedMinutes).padStart(2, "0");
+//     return `${formattedHours}:${paddedMinutes}`;
+//   };
+
+//   return (
+//     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
+//       {gigs.map((gig: any) => (
+//         <div key={gig.id}>
+//           <Card className="w-full">
+//             <CardHeader>
+//               <CardTitle>{gig.title}</CardTitle>
+//             </CardHeader>
+//             <CardContent>{gig.content}</CardContent>
+//             <CardContent>
+//               {formatTimeInterval(gig.Interval.hours, gig.Interval.minutes)}
+//             </CardContent>
+//             <CardContent>Accepted by - {gig.acceptedbyId?gig.acceptedbyId:"0"}</CardContent>
+//             <CardFooter>{new Date(gig.startDateTime).toLocaleString().slice(0,8)} to {new Date(gig.endDateTime).toLocaleString().slice(0,8)}</CardFooter>
+//             <CardFooter>Status-<h5 className={gig.status=="ACCEPTED"?"text-green-600":"text-blue-600"}>{gig.status}</h5></CardFooter>
+//           </Card>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+import { getServerSession } from "next-auth/next";
+import GigStructure from "../../components/mygigs/Gig";
+import { authOptions } from "../../lib/auth";
+import prisma from "@repo/db/client";
+import getgigs, { getAllgigs } from "../../lib/actions/getgigs";
+import { UserRole } from "@prisma/client";
+import { GigStatus } from "@prisma/client";
+import GigStructurecopy from "../../components/mygigs/Gigcopy";
+
+export interface GigsInterface {
+  id: string;
+  title: string;
+  content: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  authorId: string;
+  acceptedById: string | null;
+  Interval: any; //as it is a JSON vlaue
+  status: GigStatus;
+}
+
+export default async function Page() {
+  const gigs: GigsInterface[] = await getgigs();
   console.log(gigs);
-
-  const formatTimeInterval = (hours: number, minutes: number) => {
-    const totalMinutes = Math.floor(hours * 60 + minutes); // Convert hours to minutes and add the extra minutes
-    const formattedHours = Math.floor(totalMinutes / 60); // Calculate total hours
-    const formattedMinutes = totalMinutes % 60; // Get the remainder for minutes
-    const paddedMinutes = String(formattedMinutes).padStart(2, "0");
-    return `${formattedHours}:${paddedMinutes}`;
-  };
-
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
-      {gigs.map((gig: any) => (
-        <div key={gig.id}>
-          <Card className="w-full">
-            <CardHeader>
-              <CardTitle>{gig.title}</CardTitle>
-            </CardHeader>
-            <CardContent>{gig.content}</CardContent>
-            <CardContent>
-              {formatTimeInterval(gig.Interval.hours, gig.Interval.minutes)}
-            </CardContent>
-            <CardContent>Accepted by - {gig.acceptedbyId?gig.acceptedbyId:"0"}</CardContent>
-            <CardFooter>{new Date(gig.startDateTime).toLocaleString().slice(0,8)} to {new Date(gig.endDateTime).toLocaleString().slice(0,8)}</CardFooter>
-            <CardFooter>Status-<h5 className={gig.status=="ACCEPTED"?"text-green-600":"text-blue-600"}>{gig.status}</h5></CardFooter>
-          </Card>
-        </div>
-      ))}
+    <div>
+      <GigStructurecopy gigs={gigs}/>
     </div>
   );
 }
---
+++ my-turborepo/apps/skizify/app/components/mygigs/Bentogridcopy.tsx
@@ -0,0 +1,89 @@
+import { send } from "process";
+import { cn } from "../../utils/cn";
+
+export const BentoGridcopy = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto grid max-w-7xl grid-cols-1 gap-4 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItemcopy= ({
+  className,
+  title,
+  description,
+  header,
+  icon,
+  sender,
+  range,
+  status,
+  interval,
+  acceptedby,
+}: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  header?: React.ReactNode;
+  icon?: React.ReactNode;
+  sender?: string | React.ReactNode;
+  range?: string | React.ReactNode;
+  status?: string | React.ReactNode;
+  interval?: any | React.ReactNode;
+  acceptedby?: string | React.ReactNode;
+}) => {
+  const formatTimeInterval = (hours: number, minutes: number) => {
+        const totalMinutes = Math.floor(hours * 60 + minutes); // Convert hours to minutes and add the extra minutes
+        const formattedHours = Math.floor(totalMinutes / 60); // Calculate total hours
+        const formattedMinutes = totalMinutes % 60; // Get the remainder for minutes
+        const paddedMinutes = String(formattedMinutes).padStart(2, "0");
+        return `${formattedHours}:${paddedMinutes}`;
+      }
+  return (
+    <div
+    className={cn(
+      "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-transparent bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+      className
+    )}
+  >
+       {header}
+      <div className="transition duration-200 group-hover/bento:translate-x-2">
+        <div className="flex items-center gap-4">
+          <div>{icon}</div>
+          <div className="font-semibold text-neutral-600 dark:text-neutral-200">
+            {sender}
+          </div>
+        </div>
+        <div className="mt-2 font-sans text-lg font-bold text-neutral-800 dark:text-neutral-200">
+          {title}
+        </div>
+        <div className="mt-1 font-sans text-sm text-neutral-600 dark:text-neutral-300">
+          {description}
+        </div>
+        <div className="mt-1 font-sans text-sm text-neutral-600 dark:text-neutral-300">
+          <strong>Range: </strong>{range}
+        </div>
+        <div className="mt-1 font-sans text-sm text-neutral-600 dark:text-neutral-300">
+          <strong>Interval: </strong>{formatTimeInterval(interval.hours, interval.minutes)}
+        </div>
+        <div className="mt-1 font-sans text-sm text-neutral-600 dark:text-neutral-300">
+          <strong>Accepted by: </strong>{acceptedby}
+        </div>
+        <div className="mt-1 font-sans text-sm text-neutral-600 dark:text-neutral-300">
+          <strong>Status: </strong>{status}
+        </div>
+      </div>
+    </div>
+  );
+};
