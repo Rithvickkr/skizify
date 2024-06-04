2aa0109839c6361192877194e22a79a24f480339
Rithvickkr <12216061@nitkkr.ac.in>
2024-06-05 00:37:27 +0530

    refactor: Update Home component to fetch and display gigs
    This commit modifies the `page.tsx` file in the `mygigs` directory of the `skizify` app. The `Home` component is updated to fetch gigs using the `getgigs` function and display them in a list format. The code for rendering individual gig cards is commented out for now.
    
    Note: This commit message follows the established convention of using the "refactor" type for code improvements.

--- my-turborepo/apps/skizify/app/(dashboard)/mygigs/page.tsx
+++ my-turborepo/apps/skizify/app/(dashboard)/mygigs/page.tsx
@@ -1,8 +1,29 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../../@/components/ui/card";
+import getgigs from "../../lib/actions/getgigs";
 
-export default function Home(){
-    return (
-        <div>
-            You have visited page
-        </div>
-    )
+export default async function Home() {
+  const gigs: any = await getgigs();
+  console.log(gigs);
+ 
+return ( gigs.map((gig: any) => {
+
+  return (
+    <div>
+     {/* <Card className="w-[350px]" key={gig.id}>
+          <CardHeader>
+            <CardTitle>{gig.title}</CardTitle>
+          </CardHeader>
+          <CardContent>{gig.content}</CardContent>
+          <CardFooter>{gig.startDateTime}</CardFooter>
+        </Card> */}
+    </div>
+  );
+}));
 }
