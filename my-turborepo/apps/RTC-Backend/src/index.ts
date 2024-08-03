ae354f56ccd685e8936623db165dd9e937385662
Yash <139779840+yashop7@users.noreply.github.com>
2024-08-03 12:52:21 +0530

    weeert

--- my-turborepo/apps/RTC-Backend/src/index.ts
+++ my-turborepo/apps/RTC-Backend/src/index.ts
@@ -8,7 +8,7 @@ let receiverSocket: WebSocket | null = null;
 wss.on("connection", function connection(ws) {        
 
 
-  ws.on("erroewegergljfnr", console.error); 
+  ws.on("erewferoewegergljfnr", console.error); 
   ws.on("message", function (data: any) {
     //data in the form of String
     const message = JSON.parse(data);
