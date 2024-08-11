63e1747b788b0237189e51dd32f860f583180d99
Rithvickkr <12216061@nitkkr.ac.in>
2024-08-12 00:38:03 +0530

    some minor fixes in form (not imp)

--- my-turborepo/apps/skizify/app/(dashboard)/postgig/page.tsx
+++ my-turborepo/apps/skizify/app/(dashboard)/postgig/page.tsx
@@ -2,17 +2,15 @@ import { GigForm } from "../../components/Gigform";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className=" mx-auto p-4">
-        <div className="max-w-lg w-full bg-white shadow-md rounded-lg overflow-hidden">
-          <div className="p-4">
-            <h2 className="text-2xl font-semibold mb-4 text-center">Gig Form</h2>
-            <div className="flex justify-center">
+    <div className="flex justify-center items-center min-h-screen bg-cyan-800 ">
+      <div className=" mx-auto ">
+        <div className="max-w-lg w-full bg-white overflow-hidden dark:bg-[#020817] shadow-md rounded-lg ">
+          <div className="flex justify-center">
               <GigForm />
             </div>
           </div>
         </div>
       </div>
-    </div>
+    
   );
 }
