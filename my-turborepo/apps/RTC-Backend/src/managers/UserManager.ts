8ae73aaeb65ee3652cd89042874cb8e18420edab
Yash <139779840+yashop7@users.noreply.github.com>
2024-08-12 20:39:19 +0530

    giu3gr34yfyu

--- my-turborepo/apps/RTC-Backend/src/managers/UserManager.ts
+++ my-turborepo/apps/RTC-Backend/src/managers/UserManager.ts
@@ -107,6 +107,7 @@ export class UserManager {
         );
       }
     );
+    
     UserSocket.on("send-message", ({ message }: { message: string }) => {
       console.log("Yeah GOT the message, Sending on Particular Room Id");
       console.log("Broadcasting to room:", meetingId);
