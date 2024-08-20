12fb168305579cc9e75e40d64cdb506e18cbdce7
Rithvickkr <12216061@nitkkr.ac.in>
2024-08-20 18:45:51 +0530

    refactor: Remove unnecessary background color in postgig page

--- my-turborepo/apps/skizify/app/(dashboard)/postgig/page.tsx
+++ my-turborepo/apps/skizify/app/(dashboard)/postgig/page.tsx
@@ -3,7 +3,7 @@ import Reviewpage from "../../components/reviewpage/review";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-cyan-800">
+    <div className="flex min-h-screen items-center justify-center">
       <div className="mx-auto">
         <div className="w-full max-w-lg overflow-hidden rounded-lg bg-white shadow-md dark:bg-[#020817]">
           <div className="flex justify-center">
