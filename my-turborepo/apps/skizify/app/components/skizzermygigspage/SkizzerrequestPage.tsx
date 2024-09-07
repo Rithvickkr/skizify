72b7de98cb0ac077ec6e2d893183106d63d4979a
Yash <139779840+yashop7@users.noreply.github.com>
2024-09-07 23:32:14 +0530

    Deleted Skizzer page

--- my-turborepo/apps/skizify/app/components/skizzermygigspage/SkizzerrequestPage.tsx
+++ my-turborepo/apps/skizify/app/components/skizzermygigspage/SkizzerrequestPage.tsx
@@ -1,6 +1,5 @@
 import { skizzerRequests } from "../../lib/actions/skizzerRequests";
 import { GigStatus } from "@prisma/client";
-import SkizzerRequestCard from "./SkizzerRequestCard";
 import EnhancedGigCards from "./SkizzzerRequestCard2";
 import { GiguserContent } from "@repo/store/types";
 
@@ -12,15 +11,6 @@ export default async function SkizzerrequestPage() {
   }
 
   return (
-    // <div className="mx-auto grid w-full grid-cols-1 gap-2 md:grid-cols-2 2xl:grid-cols-3 pl-1 md:pl-2 pr-4">
-    //     {requests?.map((request) => {
-    //         return (
-    //             <div key={request.id}>
-    //                 <SkizzerRequestCard request={request}/>
-    //             </div>
-    //         )
-    //     })}
-    // </div>
     <div className="min-h-screen bg-gradient-to-br from-neutral-100 via-neutral-200 to-neutral-300 p-4 dark:from-black dark:to-neutral-900 sm:p-8">
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 4xl:grid-cols-5">
         {requests?.map((gig) => <EnhancedGigCards key={gig.id} gig={gig} />)}
