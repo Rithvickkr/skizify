3ac0f152611300c670bd8de9849e9b215328ec4e
Rithvickkr <12216061@nitkkr.ac.in>
2024-09-11 17:57:13 +0530

    rtere7y

--- my-turborepo/apps/skizify/app/components/skizzermygigspage/SkizzerrequestPage.tsx
+++ my-turborepo/apps/skizify/app/components/skizzermygigspage/SkizzerrequestPage.tsx
@@ -3,7 +3,7 @@ import EnhancedGigCards from "./SkizzzerRequestCard2";
 import { GiguserContent } from "@repo/store/types";
 
 export default async function SkizzerrequestPage() {
-  const requests = await skizzerRequests();
+  const requests: GiguserContent[] = await skizzerRequests() as GiguserContent[];
   console.log(requests);
   if (requests?.length === 0) {
     return <div>No requests are there to show you</div>;
@@ -12,7 +12,7 @@ export default async function SkizzerrequestPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-neutral-100 via-neutral-200 to-neutral-300 p-4 dark:from-black dark:to-neutral-900 sm:p-8">
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 4xl:grid-cols-5">
-        {requests?.map((gig) => <EnhancedGigCards key={gig.id} gig={gig} />)}
+        {requests?.map((gig : GiguserContent) => <EnhancedGigCards key={gig.id} gig={gig} />)}
       </div>
     </div>
   );
