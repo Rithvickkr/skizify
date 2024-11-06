7ab6cc9f271e78e0c9f24f1d0090256bcfe7cc60
Rithvickkr <12216061@nitkkr.ac.in>
2024-11-07 00:53:18 +0530

    Add skizzer field to User model and update profile page
    - Add skizzer field to the User model in the database schema
    - Update the profile page to import and use the MyProfileSection component
    - Modify the setformy function to set the skizzer field to true

--- my-turborepo/apps/skizify/app/(dashboard)/profile/page.tsx
+++ my-turborepo/apps/skizify/app/(dashboard)/profile/page.tsx
@@ -1,9 +1,10 @@
+import MyProfileSection from "../../components/Myprofilepage";
 import Newprofile from "../../components/Profile2new";
 
 export default function ProfilePage() {
   return (
     <div>
-      <Newprofile isVisible={true}  />
+      <MyProfileSection />
     </div>
   );
 }
---
+++ my-turborepo/packages/db/prisma/migrations/20241106191822_skiizertrueadded/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "skizzer" BOOLEAN NOT NULL DEFAULT false;
