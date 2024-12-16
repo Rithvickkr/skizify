d4036601a54ca0a92482f4ffe181124a912c169c
Yash <139779840+yashop7@users.noreply.github.com>
2024-12-17 00:58:46 +0530

    Code chanmge of footer and Signin

--- my-turborepo/apps/skizify/app/components/footer/Footer.tsx
+++ my-turborepo/apps/skizify/app/components/footer/Footer.tsx
@@ -21,7 +21,16 @@ import {
 } from "../../../@/components/ui/form";
 
 import { Input } from "../../../@/components/ui/input";
-import { BottomGradient } from "../SignupForm";
+// import BottomGradient  from "../SignupForm";
+
+export const BottomGradient = () => {
+  return (
+    <>
+      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
+    </>
+  );
+};
 import { Button } from "../ui/button";
 import { Section } from "./craft";
 const formSchema = z.object({
