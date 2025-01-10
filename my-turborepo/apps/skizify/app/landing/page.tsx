a269d2c624412eaa3f910216cee3ecfb298fb9ed
Yash <139779840+yashop7@users.noreply.github.com>
2025-01-10 17:05:48 +0530

    Add footer components and integrate new styling for improved layout

---
+++ my-turborepo/apps/skizify/app/landing/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-white dark:bg-black">
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+    </div>
+  );
+}
