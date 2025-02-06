1cb3d03ff43e8c933ed06faedce32fa9c1131325
Rithvickkr <12216061@nitkkr.ac.in>
2025-02-06 23:12:40 +0530

    Revert "Refactor Landing components: reorganize HoverCard and BentoGridLandingPage, update imports, and enhance MacOSWindow layout"
    This reverts commit a130eb6ced372311e3629049811da93acef8234f.

--- my-turborepo/apps/skizify/app/home/page.tsx
+++ my-turborepo/apps/skizify/app/home/page.tsx
@@ -1,5 +1,5 @@
 import { Text } from "lucide-react";
-import BentoGridLandingDemo from "../components/Landing/Bento/BentoGridLandingPage";
+import BentoGridLandingDemo from "../components/Landing/BentoGridLandingPage";
 import NavbarLanding from "../components/Landing/Navbar";
 import { TextRevealDemo } from "../components/Landing/TextReveal";
 import { Robot } from "../components/Landing/Robot";
@@ -11,7 +11,7 @@ import { BentoLanding } from "../components/Landing/BentoLanding";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen bg-black w-screen overflow-x-hidden">
+    <main className="relative min-h-screen bg-[#121212] w-screen overflow-x-hidden">
         <NavbarLanding />
       <Frontpage />
       <MacOSWindow />
