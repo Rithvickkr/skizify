56e9b78e25c8517b78ec1ffed3ded72d06964567
Rithvickkr <12216061@nitkkr.ac.in>
2025-02-09 19:23:54 +0530

    Temporarily disable Datepick component

--- my-turborepo/apps/skizify/app/components/Datepick.tsx
+++ my-turborepo/apps/skizify/app/components/Datepick.tsx
@@ -1,31 +1,31 @@
-"use client";
-import React from "react";
-import { DateRangePicker } from "@nextui-org/date-picker";
-import { DateValue, parseDateTime } from "@internationalized/date";
-import { RangeValue } from "@react-types/shared";
+// "use client";
+// import React from "react";
+// import { DateRangePicker } from "@nextui-org/date-picker";
+// import { DateValue, parseDateTime } from "@internationalized/date";
+// import { RangeValue } from "@react-types/shared";
 
-export default function Datepick() {
-  let [date, setDate] = React.useState<RangeValue<DateValue> | null>({
-    start: parseDateTime("2024-04-01T18:45:22"),
-    end: parseDateTime("2024-04-08T19:15:22"),
-  });
+// export default function Datepick() {
+//   let [date, setDate] = React.useState<RangeValue<DateValue> | null>({
+//     start: parseDateTime("2024-04-01T18:45:22"),
+//     end: parseDateTime("2024-04-08T19:15:22"),
+//   });
 
-  return (
-    <div className="flex flex-col items-start gap-4">
-      <DateRangePicker
-        fullWidth
-        granularity="second"
-        label="Date and time range"
-        value={date}
-        onChange={(value: RangeValue<DateValue> | null) =>
-          setDate(
-            value ?? {
-              start: parseDateTime("0000-01-01T00:00:00"),
-              end: parseDateTime("9999-12-31T23:59:59"),
-            }
-          )
-        }
-      />
-    </div>
-  );
-}
+//   return (
+//     <div className="flex flex-col items-start gap-4">
+//       <DateRangePicker
+//         fullWidth
+//         granularity="second"
+//         label="Date and time range"
+//         value={date}
+//         onChange={(value: RangeValue<DateValue> | null) =>
+//           setDate(
+//             value ?? {
+//               start: parseDateTime("0000-01-01T00:00:00"),
+//               end: parseDateTime("9999-12-31T23:59:59"),
+//             }
+//           )
+//         }
+//       />
+//     </div>
+//   );
+// }
